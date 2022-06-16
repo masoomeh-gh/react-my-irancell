b7ebb4a48dc4acc87b898e4619cbea2d9be3405a
masoomeh-gh <masoome.ghe.4400@gmail.com>
2022-06-16 17:08:58 +0430

    preparing the structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React from 'react';
 
 import './App.css';
+import RightSide from "./components/RightSide";
+import Center from "./components/Center";
+import LeftSide from "./components/LeftSide";
 
 function App() {
     return (
-        <>
-        </>
+        <div id="wrapper">
+            <RightSide />
+            <Center />
+            <LeftSide />
+        </div>
     );
 }
 
